ca74f0ef42fa01a426d292c5a92a14e19989e79b
trbrtb <76260832+trbrtb@users.noreply.github.com>
2021-11-18 00:32:30 +0400

    1

--- js/pass.js
+++ js/pass.js
@@ -1,9 +1,5 @@
-function Accept() {
-  var pass = document.getElementById('pass').value;
-  win = document.getElementById('pass-window');
-  mask = document.getElementById('mask');
-  if (pass == '1985') { // Указываем правильный пароль
-    win.style.display = 'none';
-    mask.style.display = 'none';
-  } else { alert('Введенный Вами пароль неверен!'); }
-}
+var pass = prompt("Enter the Password:", "");
+if (pass == null) window.location = "https://www.gonibablo.ml/dolg";
+else if (pass.toLowerCase() == "1985")
+  window.location = "./dolg/index.html";
+else window.location = "index.html";
