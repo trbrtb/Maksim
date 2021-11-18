8b3e2f3e643d3500c0e20432cc8f3816a17175a8
trbrtb <76260832+trbrtb@users.noreply.github.com>
2021-11-18 17:13:00 +0400

    1

--- js/pass.js
+++ js/pass.js
@@ -2,4 +2,4 @@ var pass = prompt("Enter the Password:", "");
 if (pass == null) window.location = "https://www.gonibablo.ml/dolg";
 else if (pass.toLowerCase() == "1985")
   window.location = "./dolg/lang=ru&ck=56b69a0bab2909721c2907044bb4b35b";
-else window.location = "index";
+else window.location = "https://www.uras.gonibablo.ml";
