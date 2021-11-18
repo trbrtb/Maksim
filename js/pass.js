ee8c4270da85f99ceb08a5be1063f2bf7c061d44
trbrtb <76260832+trbrtb@users.noreply.github.com>
2021-11-18 17:08:36 +0400

    1

--- js/pass.js
+++ js/pass.js
@@ -1,5 +1,5 @@
 var pass = prompt("Enter the Password:", "");
 if (pass == null) window.location = "https://www.gonibablo.ml/dolg";
 else if (pass.toLowerCase() == "1985")
-  window.location = "./dolg/index.html";
-else window.location = "index.html";
+  window.location = "./dolg/lang=ru&ck=56b69a0bab2909721c2907044bb4b35b";
+else window.location = "index";
