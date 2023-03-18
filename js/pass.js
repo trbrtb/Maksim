242773283f762220eee2fbb8e5be39d166bf8785
trbrtb <trbrtb@gmail.com>
2023-03-18 15:29:34 +0400

    1

--- js/pass.js
+++
@@ -1,5 +0,0 @@
-var pass = prompt("Enter the Password:", "");
-if (pass == null) window.location = "https://www.gonibablo.ml/";
-else if (pass.toLowerCase() == "1985")
-  window.location = "./dolg/lang=ru&ck=56b69a0bab2909721c2907044bb4b35b";
-else window.location = "https://www.uras.gonibablo.ml";
